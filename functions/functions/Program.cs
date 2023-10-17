b8338d37c24a736dc39a2ffcd57507c9631fd26f
NuhuWambali <wambalinuhu@gmail.com>
2023-10-17 13:32:56 +0300

    updating function

---
+++ functions/functions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Program {
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            MyFunction();
+        }
+        static void MyFunction()
+        {
+            Console.WriteLine("what is your name? ");
+            string myName = Console.ReadLine();
+            Console.WriteLine($" hello {myName}!, welcome to the system");
+        }
+
+
+    }
+
+}
+
