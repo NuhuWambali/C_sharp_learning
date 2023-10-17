1e6711d53e129af8c7780faedec2164b588eb609
NuhuWambali <wambalinuhu@gmail.com>
2023-10-17 15:37:37 +0300

    practising inheritance in C#

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -40,24 +40,58 @@ namespace Program
 
 
 //private object protected
-using System;
+
+/* using System;
 
 namespace Program
 {
     class User
     {
         private string name = "Nuhu";
+        string email = "wambalinuhu@gmail.com";  
 
     }
 
     class Program
     {
         static void Main(string[] args)
-        {
-            User myObj = new User();
+        
+            User myObj = new User(); 
             Console.WriteLine(myObj.name);
+            Console.WriteLine(myObj.email);  
         }
     }
 
 }
 
+*/
+
+
+using System;
+namespace Program
+{
+    class Vehicle
+    {
+        public string color = "green";
+        public void Honk()
+        {
+            Console.WriteLine(" Hooon! Hooon! Hooon!");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public string carModel = "Nissan";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            Console.WriteLine(myCar.carModel);
+            Console.WriteLine(myCar.color);
+            myCar.Honk();
+        }
+    }
+}
