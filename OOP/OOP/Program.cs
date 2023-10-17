c81ae9dbdeb42ab77223c4f97c3e223ced09f21d
NuhuWambali <wambalinuhu@gmail.com>
2023-10-17 13:49:36 +0300

    object oriented programming

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Program {
+   class Program
+    {
+        string color = "red";
+        static void Main(string[] args)
+        {
+            Program myObj = new Program();
+            Console.WriteLine(myObj.color);
+        }
+    }
+}
