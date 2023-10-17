a23d63abfee6326832da5fc9e5a843bb8f8b18ba
NuhuWambali <wambalinuhu@gmail.com>
2023-10-17 12:41:26 +0300

    updating arrays

---
+++ arrays/arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //decraling an array
+            string[] cars = {"nissan", "toyota", "Mitsubish", "Tesla"};
+            Console.WriteLine(cars[3]);
+            //change an array element
+            cars[0] = "Range";
+            //Console.WriteLine(cars[0]);
+          /*  for(int i=0; i<cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }*/
+          foreach(string i in cars)
+            {
+                Console.WriteLine(i);
+            }
+        } 
+    }
+}
