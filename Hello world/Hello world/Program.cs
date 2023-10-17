95ffc4844e291d7816e046815a241b67f68f8d55
NuhuWambali <wambalinuhu@gmail.com>
2023-10-17 09:41:56 +0300

    initial commit

---
+++ Hello world/Hello world/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO THE SYSTEM\n please enter your name");
+            string myName = Console.ReadLine();
+            Console.WriteLine("Your name is "+myName);
+        }
+    }
+}
