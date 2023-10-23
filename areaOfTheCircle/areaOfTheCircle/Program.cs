b69d0f2803c5e620d80c671aeff1db6608ed1d55
NuhuWambali <wambalinuhu@gmail.com>
2023-10-23 17:47:01 +0300

    area of the circle

--- areaOfTheCircle/areaOfTheCircle/Program.cs
+++ areaOfTheCircle/areaOfTheCircle/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-namespace AreaOfTheCirle
+namespace AreaOfTheCircle
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the radius of the circle");
             const double pi = 3.14;
-            Console.WriteLine("enter the radius of the cirle in meter");
             double radius = Convert.ToDouble(Console.ReadLine());
-            double areaCirle = pi * radius * radius;
-            Console.WriteLine(" radius : {0} and the area is {1}",radius,areaCirle);
+            double areaOfTheCircle= pi * radius * radius;
+            Console.WriteLine("radius is {0}, and area is {1}", radius, areaOfTheCircle);
         }
     }
 }
