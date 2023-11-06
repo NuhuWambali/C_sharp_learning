545394f3a42f0f247886529b91f196711c115a0f
NuhuWambali <wambalinuhu@gmail.com>
2023-11-06 11:45:58 +0300

    learning method with parameters

---
+++ methodWithParam/methodWithParam/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Random random = new Random();
+int randomNumber = random.Next(1000, 3000);
+
+CountTo(randomNumber);
+Console.WriteLine("flew away");
+
+
+void CountTo(int max)
+{
+    for(int i=0; i<max; i++)
+    {
+        Console.WriteLine(@"$"+i);
+      
+    }
+}
