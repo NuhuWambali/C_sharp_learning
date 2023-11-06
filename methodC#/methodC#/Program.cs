710d507ed5075ba42994dee9443843c84395fcd1
NuhuWambali <wambalinuhu@gmail.com>
2023-11-06 11:27:35 +0300

    working on methods

---
+++ methodC#/methodC#/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] a = { 1, 2, 3, 4, 5 };
+int[] b= { 100, 200,300 };
+
+Console.WriteLine("Contents of Array:");
+PrintArray();
+PrintNames();
+
+void PrintArray()
+{
+    foreach (int x in a)
+    {
+        Console.Write($"{x} ");
+    }
+    Console.WriteLine();
+}
+
+
+void PrintNames()
+{
+    foreach(int x  in b)
+    {
+        Console.WriteLine(x);
+    }
+    Console.WriteLine();
+}
